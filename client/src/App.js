a08603081568c261e3fce7aeddbef50bdd83a3d9
robertnjunge <mubearobert@gmail.com>
2023-11-29 18:09:32 +0300

    Updated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
-import Login from "./components/Login";
 import { Route,Routes} from 'react-router-dom';
-import Registration from "./components/Registration";
+import Signin from "./components/Signin";
+import Home from './components/Home';
+import Landing from './components/Landing';
+import NavBar from './components/Navbar';
+import About from './components/About';
+import Register from './components/Register';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import ForgotPassword from "./components/Forgotpassword";
 import TermsAndConditions from "./components/Termsandconditions";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 function App() {
     return(
         <div>
+            <NavBar />
             <Routes>
-                <Route path ="/login" element={<Login/>}></Route>
-                <Route path="/registration" element={<Registration/>}></Route>
+                <Route path="/" element={<Landing/>} />
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/login" element={<Signin />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/about" element={<About />} />
                 <Route path="/forgot" element={<ForgotPassword/>}></Route>
                 <Route path="/terms and conditions" element={<TermsAndConditions/>}></Route>
                 <Route path="privacy-policy" element={<PrivacyPolicy/>}></Route>
             </Routes>
+            <Footer />
         </div>
         
     )
