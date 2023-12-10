30e420bfdde03e65c4e9c2f42d05e169708fe17b
Anzal Abdinoor <138122940+anzal-abdi56@users.noreply.github.com>
2023-12-10 13:17:26 +0300

    Implement dynamic form visibility toggle and asset submission handling (#10)
    Co-authored-by: + <+>

--- client/src/AdminViews/Admin.jsx
+++ client/src/AdminViews/Admin.jsx
@@ -4,12 +4,10 @@ import TransactionList from '../components/TransactionsList'
 import AllAssignments from '../components/AssignmentList'
 import Manager from "../components/dashboard/Manager"
 import "../Styles/manager.css"
-import AssetForm from '../components/AssetForm'
 import AssignmentComponent from '../components/AssignmentForm'
 function AdminDashboard() {
   return (
     <section >
-      <AssetForm/>
       <AssetList />
       <TransactionList/>
       <AssignmentComponent/>
--- client/src/containers/Admin.jsx
+++ client/src/containers/Admin.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import AssetList from '../components/AssetList'
 import TransactionList from '../components/TransactionsList'
 import AllAssignments from '../components/AssignmentList'
+import AssetForm from '../components/AssetForm'
 
 function AdminDashboard() {
   return (
     <div>
+      <AssetForm />
       <AssetList />
       <TransactionList />
       <AllAssignments />  
